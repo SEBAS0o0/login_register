db8fb0cf9893f44c2560fb6e6a91108e1871435b
SEBAS0o0 <92197217+SEBAS0o0@users.noreply.github.com>
2022-07-10 18:05:31 -0500

    Add files via upload

---
+++ php/conexion_be.php
@@ -0,0 +1,16 @@
+
+<?php
+    
+   $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+
+
+   /*
+   if($conexion){
+    echo 'conectado exitosamente a la base de datos';
+   }else{
+    echo'no se ha conectado a la Base de datos';
+   }
+   
+   */
+   
+?>
